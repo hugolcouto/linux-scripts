b1c75b55f615c953a9f190f602e2d18fe9d2bd23
Hugo Couto <contato@hugocouto.dev>
2021-11-05 09:07:41 -0300

    Revisão geral dos scripts

--- php7.3.sh
+++ php7.3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ondrej/php &&
-sudo apt-get update &&
-sudo apt-get install php7.3 &&
-sudo apt-get install php7.3-mbstring php7.3-xml php7.3-simplexml php7.3-dom 
+sudo add-apt-repository ppa:ondrej/php;
+sudo apt-get update;
+sudo apt-get install php7.3;
+sudo apt install php7.3 php7.3-intl php7.3-mysql php7.3-sqlite3 \
+	php7.3-gd php7.3-mbstring php7.3-xml php7.3-simplexml php7.3-dom \
+	php7.3-curl php7.3-zip
--- php8.sh
+++ php8.sh
@@ -3,4 +3,6 @@
 sudo apt-get install software-properties-comm;
 sudo add-apt-repository ppa:ondrej/php;
 sudo apt update;
-sudo apt install php8.0 php8.0-intl php8.0-mysql php8.0-sqlite3 php8.0-gd php8.0-mbstring php8.0-xml php8.0-simplexml php8.0-dom php8.0-curl php8.0-zip
+sudo apt install php8.0 php8.0-intl php8.0-mysql php8.0-sqlite3 \
+	php8.0-gd php8.0-mbstring php8.0-xml php8.0-simplexml php8.0-dom \
+	php8.0-curl php8.0-zip
